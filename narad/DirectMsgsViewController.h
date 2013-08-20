ecbb6de497a0c5134304ca4fc66c6c345b3e9c29
Srijak Rijal <srijak@gmail.com>
2013-08-20 07:02:09 -0400

    direct msgs  view start

---
+++ narad/DirectMsgsViewController.h
@@ -0,0 +1,15 @@
+//
+//  DirectMsgsViewController.h
+//  narad
+//
+//  Created by srjk on 8/19/13.
+//  Copyright (c) 2013 srjk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DirectMsgsViewController : UITableViewController
+
+@property (nonatomic, strong) NSMutableArray *messages;
+
+@end
