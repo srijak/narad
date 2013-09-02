4327d3e583aa0d41e1f91001e12eb1442d1fe9ef
Srijak Rijal <srijak@gmail.com>
2013-09-02 10:25:27 -0400

    useless

--- narad/SRIMessagesViewController.m
+++ narad/SRIMessagesViewController.m
@@ -109,10 +109,40 @@
   
 	SRIConversation *conv = [self.fetchedResultsController objectAtIndexPath:indexPath];
   
+  cell.groupImg.image = [self imageFromText:@"?"];
   cell.titleLabel.text = conv.title;
   cell.lastMessageSentLabel.text = [NSString stringWithFormat:@"%@",conv.lastMessageSent];
   cell.lastMessageTextLabel.text = conv.lastMessageText;
   cell.conversation_id = [conv.topic_id integerValue];
+  
+}
+
+-(UIImage *)imageFromText:(NSString *)text
+{
+  // set the font type and size
+  UIFont *font = [UIFont systemFontOfSize:20.0];
+  CGSize size  = [text sizeWithFont:font];
+  
+  // check if UIGraphicsBeginImageContextWithOptions is available (iOS is 4.0+)
+  if (UIGraphicsBeginImageContextWithOptions != NULL)
+    UIGraphicsBeginImageContextWithOptions(size,NO,0.0);
+  else
+    // iOS is < 4.0
+    UIGraphicsBeginImageContext(size);
+  
+  // optional: add a shadow, to avoid clipping the shadow you should make the context size bigger
+  //
+  // CGContextRef ctx = UIGraphicsGetCurrentContext();
+  // CGContextSetShadowWithColor(ctx, CGSizeMake(1.0, 1.0), 5.0, [[UIColor grayColor] CGColor]);
+  
+  // draw in context, you can use also drawInRect:withFont:
+  [text drawAtPoint:CGPointMake(0.0, 0.0) withFont:font];
+  
+  // transfer image
+  UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+  UIGraphicsEndImageContext();
+  
+  return image;
 }
 
 -(UITableViewCell *)tableView:(UITableView *)_tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
