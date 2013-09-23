c4a62820d35fdf358d8e24646355785a7c3b91d4
Srijak Rijal <srijak@gmail.com>
2013-09-23 07:51:33 -0400

    add place holder text view.

---
+++ narad/SRIPlaceholderTextView.h
@@ -0,0 +1,16 @@
+//
+//  SRIPlaceholderTextView.h
+//  narad
+//
+//  Created by srjk on 9/23/13.
+//  Copyright (c) 2013 srjk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SRIPlaceholderTextView : UITextView
+
+@property(nonatomic, strong) NSString *placeholder;
+@property (nonatomic, strong) UIColor *placeholderColor;
+
+@end
---
+++ narad/SRIPlaceholderTextView.m
@@ -0,0 +1,118 @@
+//
+//  SRIPlaceholderTextView.m
+//  narad
+//
+//  Created by srjk on 9/23/13.
+//  Copyright (c) 2013 srjk. All rights reserved.
+//
+
+#import "SRIPlaceholderTextView.h"
+
+@interface SRIPlaceholderTextView ()
+
+@property (nonatomic, strong) UIColor* realTextColor;
+@property (unsafe_unretained, nonatomic, readonly) NSString* realText;
+
+- (void) beginEditing:(NSNotification*) notification;
+- (void) endEditing:(NSNotification*) notification;
+
+@end
+
+@implementation SRIPlaceholderTextView
+
+#pragma mark -
+#pragma mark Initialisation
+
+- (id) initWithFrame:(CGRect)frame {
+  if ((self = [super initWithFrame:frame])) {
+    [self awakeFromNib];
+  }
+  return self;
+}
+
+- (void)awakeFromNib {
+  [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(beginEditing:) name:UITextViewTextDidBeginEditingNotification object:self];
+  [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(endEditing:) name:UITextViewTextDidEndEditingNotification object:self];
+  
+  self.realTextColor = [UIColor blackColor];
+  self.placeholderColor = [UIColor lightGrayColor];
+}
+
+#pragma mark -
+#pragma mark Setter/Getters
+
+- (void) setPlaceholder:(NSString *)aPlaceholder {
+  if ([self.realText isEqualToString:self.placeholder] && ![self isFirstResponder]) {
+    self.text = aPlaceholder;
+  }
+  if (aPlaceholder != self.placeholder) {
+    _placeholder = aPlaceholder;
+  }
+  
+  
+  [self endEditing:nil];
+}
+
+- (NSString *) text {
+  NSString* text = [super text];
+  if ([text isEqualToString:self.placeholder]) return @"";
+  return text;
+}
+
+- (void) setText:(NSString *)text {
+  if (([text isEqualToString:@""] || text == nil) && ![self isFirstResponder]) {
+    super.text = self.placeholder;
+  }
+  else {
+    super.text = text;
+  }
+  
+  if ([text isEqualToString:self.placeholder]) {
+    self.textColor = self.placeholderColor;
+  }
+  else {
+    self.textColor = self.realTextColor;
+  }
+}
+
+- (NSString *) realText {
+  return [super text];
+}
+
+- (void) beginEditing:(NSNotification*) notification {
+  if ([self.realText isEqualToString:self.placeholder]) {
+    super.text = nil;
+    self.textColor = self.realTextColor;
+  }
+}
+
+- (void) endEditing:(NSNotification*) notification {
+  if ([self.realText isEqualToString:@""] || self.realText == nil) {
+    super.text = self.placeholder;
+    self.textColor = self.placeholderColor;
+  }
+}
+
+- (void) setTextColor:(UIColor *)textColor {
+  if ([self.realText isEqualToString:self.placeholder]) {
+    if ([textColor isEqual:self.placeholderColor]){
+      [super setTextColor:textColor];
+    } else {
+      self.realTextColor = textColor;
+    }
+  }
+  else {
+    self.realTextColor = textColor;
+    [super setTextColor:textColor];
+  }
+}
+
+#pragma mark -
+#pragma mark Dealloc
+
+- (void)dealloc {
+  [[NSNotificationCenter defaultCenter] removeObserver:self];
+  
+}
+
+@end
